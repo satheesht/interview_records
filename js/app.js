cb17544f266744c94861344009e39b6002c056e0
Satheesh Thangavel <tsatheesh@pkrmsezdc.com>
2017-08-18 19:52:23 +0530

    progress

--- js/app.js
+++ js/app.js
@@ -1,33 +1,49 @@
-var app = angular.module('tq_database', []);
+var app = angular.module('tq_database', ['ngRoute']);
 
-app.controller('tq_db_controller', function($scope,IDGenerator,storage) {
+app.config(function($routeProvider){
+	$routeProvider.
+	when("/",{templateUrl:"page_1.html",controller:"tq_db_controller",controllerAs:"c1"}).
+	when("/list",{templateUrl:"page_2.html",controller:"controller2",controllerAs:"c2"}).
+	otherwise({
+		redirectTo:"/index"
+	});
+});
+
+app.controller("tq_db_list_controller",function($scope,storage){
+
+});
+
+app.controller('tq_db_controller', function($scope,IDGenerator,storage,ta_validate) {
     $scope.form_fields = {
         val_purpose:"Visa",
         val_city: "London",
         val_address:"tset1",
         val_interviewer:"tset2",
         val_interviewee:"tset3",
-        val_interviewee_email:"tset4",
-        val_interviewee_phone:"tset5",
+        val_interviewee_email:"tset4@test.com",
+        val_interviewee_phone:"666-666-6666",
         val_date:"tset6",
         val_interview_id:""
     }
+    $scope.cities = ['','London','Brington','Belfast','Cardiff','Newcastle','Elsewhere'];
     $scope.error_on = false;
-    $scope.submit_ta_records = function(){ console.log("validation started");
-        $scope.error_on = true;
-        for (var i in $scope.form_fields) {
-            if ($scope.form_fields[i] == "") {
-                $scope.error_on = true;
-                break;
-            }
-            $scope.error_on = false;
-        }
-        if(!$scope.error_on){
+    $scope.invalid_phone = false;
+    $scope.invalid_email = false;
+    $scope.required_missing= false;
+    $scope.submit_ta_records = function(){ 
+        if($scope.validate($scope.form_fields)){
             if(!storage.store_ta_record($scope.form_fields)){
-                $scope.error_on = true;
                 $scope.form_fields.val_interview_id = IDGenerator.generateInterviewID();
             }
+        }else{
+            $scope.error_on = true;
         }
+    }
+    $scope.validate = function(records){
+        ta_validate.checkPhone(records.val_interviewee_phone)?$scope.invalid_phone=false:$scope.invalid_phone=true;
+        ta_validate.checkEmail(records.val_interviewee_email)?$scope.invalid_email=false:$scope.invalid_email=true;
+        ta_validate.checkRequired(records)?$scope.required_missing = false:$scope.required_missing = true;
+        return !($scope.required_missing || $scope.invalid_email || $scope.invalid_phone);
     }
     $scope.generateID = function(){
        $scope.form_fields.val_interview_id = IDGenerator.generateInterviewID();
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,6 @@
 app.service('IDGenerator',function(){
     this.generateInterviewID = function(){
-        var id = "#"+Math.random().toString(15).slice(2);
+        var id = "#"+Math.random().toString(12).slice(2).substr(0,13);
         if(!this.is_id_redundant(id)){
             return id;
         }else this.generateInterviewID(); // Recursive function call
@@ -26,5 +26,36 @@ app.service('storage',function(){
             ta_records.push(records);
             localStorage.setItem("ta_records",JSON.stringify(ta_records));
             console.log(JSON.parse(localStorage.getItem("ta_records")));
+    }
+});
+
+app.service('ta_validate',function(){
+
+    this.checkRequired = function(values){
+        for (var i in values) {
+            if (values[i] == "") {
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+    this.checkPhone = function(number){
+        if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(number)){  
+            return true;  
+        } 
+        return false;    
+    }
+    this.checkEmail = function(email){
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){  
+            return true;  
+        }
+        return false;
+    }
+    this.checkInterviewID = function(id){
+        if(/^([a-zA-Z0-9 _-]+)$/.test(id)){
+            return true;
+        }
+        return false;
     }
 });
